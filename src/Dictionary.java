f5afcc5360e1ab6f9aa56ce599218fa4404c709a
asharameta <1sharameta@gmail.com>
2024-07-15 19:03:53 +0200

    2 apis with word dictionary, method for request random word

---
+++ src/Dictionary.java
@@ -0,0 +1,40 @@
+package src;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
+
+public class Dictionary {
+	private final String wordUri = "https://random-word-api.herokuapp.com/word";
+	
+	public Dictionary() {}
+	
+	public CompletableFuture<String> getAsyncRandomWord() {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(wordUri))
+                .build();
+        
+        return client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body);
+	}
+	
+	public String getSyncRandomWord() {
+        String result = "";
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(wordUri))
+                .build();
+        
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            result = response.body();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return result;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
 package src;
 
+
+
+///word api that can be used
+///https://random-word-api.herokuapp.com/home
+///https://www.datamuse.com/api/
+
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.print("Hello world again!");
+		System.out.println("Hello world again!");
+		
+		Dictionary dic = new Dictionary();
+		
+		String result = dic.getAsyncRandomWord()
+        .join();
+		
+		//String result = dic.getSyncRandomWord();
+		
+		int wordLength = result.length();
+		
+		System.out.println(result.substring(2, wordLength-2));
 	}
 }
