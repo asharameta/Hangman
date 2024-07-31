0ef06f3b747044d446b110961d498ac45b17138f
asharameta <1sharameta@gmail.com>
2024-07-31 21:17:27 +0200

    some code optimization

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,35 +1,39 @@
 package src;
 
+import java.util.Scanner;
+
 public class Hangman {
-	private Dictionary dic = new Dictionary();
-	private final String wordToGuess = dic.getSyncRandomWord();
-	private final String cleanedWord = wordToGuess.substring(2, wordToGuess.length()-2);
-	private final int wordLength = cleanedWord.length();
+	private Dictionary dic;
+	public Hangman() {
+		dic = new Dictionary();
+	}
 
 	private static final String RESET = "\u001B[0m";
 	private static final String GREEN = "\u001B[32m";
 	private static final String YELLOW = "\u001B[33m";
 	
-	public String getTheWord() {
+	private static int ATTEMPTS = 3;
+	private static int usedAttempts = 0;
+	
+	
+	private String DrawWord() {
+		String wordToGuess = dic.getSyncRandomWord();
+		String cleanedWord = wordToGuess.substring(2, wordToGuess.length()-2);
 		return cleanedWord;
 	}
 	
-	public int getWordLength() {
-		return wordLength;
-	}
-	
-	public String CheckTheWord(String word){
-		if(cleanedWord.length() != word.length()) return "your word is less/more characters than word you have to guess";
+	private String CheckTheWord(String word, String drawnWord){
+		if(drawnWord.length() != word.length()) return "your word is less/more characters than word you have to guess";
 		
 		StringBuilder strToReturn = new StringBuilder();
-		for(int i = 0; i < cleanedWord.length(); i++){
+		for(int i = 0; i < drawnWord.length(); i++){
 			char wordChar = word.charAt(i);
-			char cleanedWordChar = cleanedWord.charAt(i);
+			char cleanedWordChar = drawnWord.charAt(i);
 			
 			if(wordChar==cleanedWordChar) {
 				strToReturn.append(GREEN+wordChar+RESET);
 			}
-			else if(cleanedWord.contains(Character.toString(wordChar))) {
+			else if(drawnWord.contains(Character.toString(wordChar))) {
 				strToReturn.append(YELLOW+wordChar+RESET);
 			}else {
 				strToReturn.append(wordChar);
@@ -38,5 +42,30 @@ public class Hangman {
 		
 		return strToReturn.toString();
 	}
+	
+	
+	public void StartGame() {
+		String drawnWord = DrawWord();
+		System.out.println("The word has " + drawnWord.length() + " letters   "+drawnWord);
+		try (Scanner scanner = new Scanner(System.in)) {
+			while(usedAttempts<ATTEMPTS) {
+				System.out.println(usedAttempts+ "/"+ ATTEMPTS);
+				System.out.print("word: ");
+				String providedWord = scanner.nextLine();
+				System.out.println(CheckTheWord(providedWord, drawnWord));
+				if(drawnWord.equalsIgnoreCase(providedWord)) {
+					System.out.println("U won! Congratz.");
+					return;
+				}
+				usedAttempts++;
+				if(usedAttempts == ATTEMPTS) {
+					System.out.println("U lost! Try again.");
+					return;
+				}
+			}
+		}
+	}
+	
+	
 	
 }
--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,9 @@
 package src;
 
-import java.util.Scanner;
-
 public class Main {
-	private static int ATTEMPTS = 3;
-	private static int usedAttempts = 0;
-	
 	public static void main(String[] args) {
 		Hangman hM = new Hangman();
-		System.out.println("Hello world again!   " + hM.getTheWord());
-		
-		System.out.println("The word has " + hM.getWordLength() + " letters");
-		try (Scanner scanner = new Scanner(System.in)) {
-			while(usedAttempts<ATTEMPTS) {
-				System.out.println(usedAttempts+ "/"+ ATTEMPTS);
-				System.out.print("word: ");
-				String providedWord = scanner.nextLine();
-				System.out.println(hM.CheckTheWord(providedWord));
-				if(hM.getTheWord().equalsIgnoreCase(providedWord)) {
-					System.out.println("U won! Congratz.");
-					return;
-				}
-				usedAttempts++;
-				if(usedAttempts == ATTEMPTS) {
-					System.out.println("U lost! Try again.");
-					return;
-				}
-			}
 			
-
-		}
+		hM.StartGame();
 	}
 }
